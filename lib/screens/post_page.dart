7846e087d2fb040cc85ca98cc28650791be1ae6d
sa8a <jpkrllll@yahoo.co.jp>
2022-12-03 02:06:14 +0900

    add: AppBarを非表示にして投稿ボタンを表示

--- lib/screens/post_page.dart
+++ lib/screens/post_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notice_board_app/widgets/empty_appbar.dart';
 
 class PostPage extends StatefulWidget {
   const PostPage({super.key});
@@ -14,40 +15,62 @@ class _PostPageState extends State<PostPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("投稿画面"),
-      ),
+      appBar: const EmptyAppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _textEditingController,
-                enabled: true, // 活性、非活性の切り替え
-                maxLength: 140, // 入力可能な文字数
-                // 入力可能な文字数の制限を超える場合の挙動を制御
-                // falseだと最大文字数を超えても入力することが可能だが見た目はエラー状態となる
-                // maxLengthEnforcement: false,
-                obscureText: false, // trueにするとパスワード入力のように値をマスクする
-                maxLines: null, // 最大行数
-                decoration: const InputDecoration(
-                  hintText: '内容を入力してください',
-                  labelText: '投稿内容',
-                  border: OutlineInputBorder(),
-                ),
-                onChanged: (String text) {
-                  // print('$text');
-                  setState(() {
-                    _postContent = text;
-                  });
-                },
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _textEditingController,
+                    enabled: true, // 活性、非活性の切り替え
+                    maxLength: 140, // 入力可能な文字数
+                    // 入力可能な文字数の制限を超える場合の挙動を制御
+                    // falseだと最大文字数を超えても入力することが可能だが見た目はエラー状態となる
+                    // maxLengthEnforcement: false,
+                    obscureText: false, // trueにするとパスワード入力のように値をマスクする
+                    maxLines: null, // 最大行数
+                    decoration: const InputDecoration(
+                      hintText: '内容を入力してください',
+                      labelText: '投稿内容',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String text) {
+                      // print('$text');
+                      setState(() {
+                        _postContent = text;
+                      });
+                    },
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              Text(_postContent),
-            ],
-          ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('キャンセル'),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: const Text('投稿する'),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/empty_appbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmptyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const EmptyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(0.0, 0.0);
+}
