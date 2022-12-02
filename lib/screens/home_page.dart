1de29ea3be2c2f106430ac10ff857641a44ee86a
sa8a <jpkrllll@yahoo.co.jp>
2022-12-03 02:36:00 +0900

    style: タイムラインのスタイルを追加

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,10 +10,39 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("掲示板アプリ"),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(20),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Center(
-          child: Text("ここに投稿を表示"),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/1594348657695854593/nYwKAqc3_400x400.jpg'),
+              ),
+              const SizedBox(width: 15),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      const Text(
+                        'さば@プログラミング勉強中',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(width: 14),
+                      Text(
+                        '2022/12/03',
+                        style: TextStyle(color: Colors.grey[600]),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  const Text('Flutter学習中！'),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
